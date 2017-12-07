2845f9423312244c08a0e545db21428a1c54b506
Jonathan Dupré <hello@jonathandupre.com>
2017-12-06 23:11:23 -0500

    assets

--- bin/cp-assets.js
+++ bin/cp-assets.js
@@ -6,7 +6,7 @@ module.exports = copyAssets
 
 async function copyAssets (done) {
   var src = path.join(__dirname, '../assets')
-  var dest = path.join(__dirname, '../dist/assets')
+  var dest = path.join(__dirname, '../dist')
 
   fs.writeFileSync(path.join(__dirname, '../dist/index.html'), fs.readFileSync(path.join(__dirname, '../src/index.html')))
 
